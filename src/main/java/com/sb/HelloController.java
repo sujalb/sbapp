8c8ba6c1ca9948c8fff387f284a3f02948277b2a
Sujal Bhat <sujalbhat@gmail.com>
2016-03-07 13:05:59 -0700

    checking in modified files

--- src/main/java/com/sb/HelloController.java
+++ src/main/java/com/sb/HelloController.java
@@ -1,7 +1,25 @@
 package com.sb;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Created by sujal on 3/3/2016.
  */
+
+@Controller
 public class HelloController {
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String hello(){
+        System.out.println("TEsting this");
+        return "Hello world";
+    }
+
+    @RequestMapping("/result")
+    public String getResultPage(){
+        return "resultPage";
+    }
 }
